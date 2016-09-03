424d0bfb4ca9e900fd3d4173873650ff52996811
tomhodev <datomster21@gmail.com>
2016-09-03 18:07:37 -0500

    add routes and actions to create, update, and delete trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,5 +10,26 @@ class TripsController < ApplicationController
     render json: @trips 
   end
 
-  
+  def create
+    @trip = user.trips.new(driver_id: params[:user_id],trips_type: params[:type])
+    if @trip.save 
+      render json: @trip, status: :created, location: @trip
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end 
+  end
+
+  def update
+    trip = Trip.find(params[:trip_id])
+    rider = User.find(params[:user_id])
+    trip.riders << rider
+    trip.save
+    rider.rides << trip
+    rider.save
+  end
+
+  def destroy
+    trip = Trip.find(params[:trip_id])
+    trip.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   post '/login', to: 'sessions#create'
   get '/trips', to: 'trips#index'
+  post '/trips', to: 'trips#create'
+  put 'trips/:trips_id', to: 'trips#update'
+  patch 'trips/:trips_id', to: 'trips#update'
+  delete 'trips/:trips_id', to: 'trips#delete'
+  
 end
 
 
