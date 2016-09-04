8551655dd653b861a7f51fe818327cb71e2862db
CJ Jessett <cjessett@gmail.com>
2016-09-04 12:48:19 -0500

    play with auth tokens

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user_from_token!
+  # skip_before_action :authenticate_user_from_token!
 
   def create
     @user = User.find_for_database_authentication(email: params[:email])
@@ -13,9 +13,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  private
-  def invalid_login_attempt
-    warden.custom_failure!
-    render json: {error: t('sessions_controller.invalid_login_attempt')}, status: :unprocessable_entity
-  end
+  # private
+  # def invalid_login_attempt
+  #   warden.custom_failure!
+  #   render json: {error: t('sessions_controller.invalid_login_attempt')}, status: :unprocessable_entity
+  # end
 end
