8dec59817237b943a6af0e5d8549f926aa0f350e
CJ Jessett <cjessett@gmail.com>
2016-09-03 23:44:48 -0500

    update spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user_from_token!
+  skip_before_action :authenticate_user_from_token!, only: [:create]
   before_action :set_user, only: [:show]
 
   def show
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe UsersController, type: :controller do
   describe 'GET #show' do
     it 'returns the users info' do
       user = create(:user)
+      auth_header = { "Authorization" => user.access_token }
 
-      get :show, params: { id: user.id }, format: :json
-
+      get :show, params: { id: user.id }, headers: auth_header
+      # binding.pry
       expect(response).to be_successful
     end
   end
+end
+
+def response_body
+  JSON.parse(response.body)
 end
