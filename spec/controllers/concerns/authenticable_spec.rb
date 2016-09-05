c47a0af1606191701604677c398f1a1d21a1ab3a
CJ Jessett <cjessett@gmail.com>
2016-09-05 13:19:06 -0500

    update Authenticable specs

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -1,23 +1,38 @@
 require 'rails_helper'
 
-class Authentication
+class Authentication < ActionController::Base
   include Authenticable
 end
 
-xdescribe Authenticable do
+describe Authenticable do
   let(:authentication) { Authentication.new }
-  subject { authentication }
 
-  describe '#current_user' do
+  describe '.current_user' do
     before do
       @user = create(:user)
-      request.headers["Authorization"] = @user.auth_token
-      # authentication.stub(:request).and_return(request)
-      allow(Authentication).to receive(:request).and_return(request)
+      request.headers['Authorization'] = @user.auth_token
+      allow(authentication).to receive(:request).and_return(request)
     end
 
     it 'should return the user from the authorization header' do
       expect(authentication.current_user.auth_token).to eq @user.auth_token
+    end
+  end
+
+  describe '.authenticate_with_token!' do
+    before do
+      allow(authentication).to receive(:current_user).and_return(nil)
+      allow(authentication).to receive(:render) do |args|
+        args
+      end
+    end
+
+    it 'returns an error' do
+      expect(authentication.authenticate_with_token![:json][:errors]).to eq "Not authenticated!"
+    end
+
+    it 'returns unauthorized status' do
+      expect(authentication.authenticate_with_token![:status]).to eq :unauthorized
     end
   end
 end
