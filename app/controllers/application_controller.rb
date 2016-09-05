672992cdde7b3926aaafaef8714ab160e11bc3a9
CJ Jessett <cjessett@gmail.com>
2016-09-05 17:24:34 -0500

    organize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,47 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
+  include Authenticable
+
   protect_from_forgery with: :null_session
 
-  include Authenticable
-  # acts_as_token_authentication_handler_for User, fallback_to_devise: false
-
   respond_to :json
-
-  # User Authentication
-  # Authenticates the user with OAuth2 Resource Owner Password Credentials Grant
-  # def authenticate_user_from_token!
-  #   auth_token = request.headers['Authorization']
-  #   if auth_token
-  #     authenticate_with_auth_token auth_token
-  #   else
-  #     authentication_error
-  #   end
-  # end
-
-  # private
-
-  # def authenticate_with_auth_token auth_token
-  #   unless auth_token.include?(':')
-  #     authentication_error
-  #     return
-  #   end
-
-  #   user_id = auth_token.split(':').first
-  #   user = User.where(id: user_id).first
-
-  #   if user && Devise.secure_compare(user.access_token, auth_token)
-  #     # User can access
-  #     sign_in user, store: false
-  #   else
-  #     authentication_error
-  #   end
-  # end
-
-  ##
-  # Authentication Failure
-  # Renders a 401 error
-  # def authentication_error
-  #   # User's token is either invalid or not in the right format
-  #   render json: {error: t('application_controller.unauthorized')}, status: 401  # Authentication timeout
-  # end
 end
