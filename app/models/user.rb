6baa65f71dd373e1bd7cca9b0674c0a6d0cd3ea6
CJ Jessett <cjessett@gmail.com>
2016-09-04 20:50:29 -0500

    update user spec with #generate_auth_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   validates_presence_of :email
   validates_uniqueness_of :email, :auth_token
 
+  before_create :generate_authentication_token!
+
   has_one :work
   has_one :home
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
