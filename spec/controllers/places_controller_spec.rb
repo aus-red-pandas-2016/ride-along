bf4b931674a5f0e6ef61fdf97c228dfc8bcd2d40
kapatton90 <kapatton19@gmail.com>
2016-09-05 23:37:47 -0500

    GET CREATE CONTROLLER TESTS TO PASS

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -1,24 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe PlacesController, type: :controller do
-	before(:each) {request.headers['Accept'] =  "#{Mime[:json]}" }
+	before(:each) do
+		@user1 = FactoryGirl.create(:user)
+		@place1 = FactoryGirl.create(:place, user:@user1)
+	end
+
+	#{request.headers['Accept'] =  "#{Mime[:json]}" }
 
 	describe 'POST #create' do
     context 'when successfully created' do
-      before(:each) do
-      	@user = create(:user, id:1)
-      	@place = create(:place)
-        @place_attributes = attributes_for(:place)
-        post :create, params: {user_id:@place.user_id, place: @place_attributes }
+    	before(:each) do
+    		@place_attributes = attributes_for(:place)
+    		post :create, params:{user_id: @place1.user_id, place: @place_attributes}
       end
+    	it "creates a new place" do
+      	expect{post :create, params:{user_id: @place1.user_id, place: @place_attributes}
+      }.to change(Place,:count).by(1)
+    	end
 
-      it 'renders json representation for the place just created' do
-      	binding.pry
-        expect(response_body[:street]).to eq @place_attributes[:street]
+    	it 'renders json representation for the place just created' do
+    		@response = JSON.parse(response.body)
+        expect(response["street"]).to eq @place_attributes[:street]
       end
-     
-     	it { should respond_with 201 }
-
-    end
+    
+    end	
   end
+
+  # describe 'PUT/PATCH #update' do
+  #   before(:each) do
+  #     @user = create(:user)
+  #     @place = create(:place)
+  #     auth_header(@user.auth_token)
+  #   end
+  # end
 end
