333c318ba742c02c33a7fa74b17713beef57f9ad
CJ Jessett <cjessett@gmail.com>
2016-09-07 18:28:59 -0500

    remove extra validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :email
   validates_uniqueness_of :email
 
   has_one :work
